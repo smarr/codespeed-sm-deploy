152080b3d1f540254102e31339161274a62f9df2
Stefan Marr <git@stefan-marr.de>
2019-11-10 18:48:18 +0000

    Update path for starter script
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- speed_unicorn.sh
+++ speed_unicorn.sh
@@ -8,10 +8,10 @@ NUM_WORKERS=3
 # user/group to run as
 USER=www-data
 GROUP=www-data
-cd /home/www-data/${NAME}
-source /home/www-data/speed/bin/activate
+cd /var/www/codespeed/${NAME}
+source /var/www/codespeed/venv/bin/activate
 test -d $LOGDIR || mkdir -p $LOGDIR
-exec /home/www-data/speed/bin/gunicorn wsgi:application -w $NUM_WORKERS \
+exec /var/www/codespeed/venv/bin/gunicorn wsgi:application -w $NUM_WORKERS \
     -b 127.0.0.1:8076 \
     --user=$USER --group=$GROUP --log-level=warning \
     --log-file=$LOGFILE 2>>$LOGFILE
