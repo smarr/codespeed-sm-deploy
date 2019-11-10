f2f38d0c6c62638f0ace21773e142ef00640bc0b
Stefan Marr <git@stefan-marr.de>
2019-11-10 18:46:58 +0000

    Update wsgi.py to match upstream version
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- awfy-speed/wsgi.py
+++ awfy-speed/wsgi.py
@@ -6,23 +6,19 @@ WSGI config
 
 import os
 import sys
-import django
 
-codespeed_dir = current_dir = os.path.abspath(os.path.dirname(__file__).replace('\\','/'))
+current_dir = os.path.abspath(os.path.dirname(__file__).replace('\\','/'))
 
 dirs = current_dir.split(os.path.sep)
-parent_folder = dirs[-1]
+del(dirs[-1])
+codespeed_dir = os.path.sep.join(dirs)
 del(dirs[-1])
 project_dir = os.path.sep.join(dirs)
 
 sys.path.append(project_dir)
 sys.path.append(codespeed_dir)
+os.environ['DJANGO_SETTINGS_MODULE'] = codespeed_dir.split(os.path.sep)[-1] + '.settings'
 
-# os.environ['DJANGO_SETTINGS_MODULE'] = parent_folder + '.settings'
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-
-django.setup()
-
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
 
